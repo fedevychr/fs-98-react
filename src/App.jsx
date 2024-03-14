c1c9d38d2be58054be7b98270147a69bb7641991
Roman Fedevych <fedevychr@gmail.com>
2024-03-14 02:43:51 +0200

    add styles, new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import MailBox from './components/MailBox';
+import MailBox from './components/MailBox/MailBox';
 
-const meestExpressUsers = [
-  { id: "1", userEmail: "user@example.com" },
-  { id: "2", userEmail: "max@examle.com" },
-  { id: "3", userEmail: "merry@examle.com" },
-];
-const novaPoshtaUsers = [
-  { id: "22", userEmail: "ddfr@examle.com" },
-  { id: "33", userEmail: "qwerty@examle.com" },
-];
-const ukrPoshtaUsers = [
-  { id: "111", userEmail: "tyfany@example.com" },
-  { id: "222", userEmail: "kirill_22@examle.com" },
-  { id: "333", userEmail: "linkoln@examle.com" },
-  { id: "3443", userEmail: "linkoln2222@examle.com" },
-];
+import meestExpressUsers from './meestExspess.json'
+import novaPoshtaUsers from './novaPoshta.json'
+import ukrPoshtaUsers from './ukrPoshta.json'
+
+// const meestExpressUsers = [
+//   { id: "1", userEmail: "user@example.com" },
+//   { id: "2", userEmail: "max@examle.com" },
+//   { id: "3", userEmail: "merry@examle.com" },
+// ];
+// const novaPoshtaUsers = [
+//   { id: "22", userEmail: "ddfr@examle.com" },
+//   { id: "33", userEmail: "qwerty@examle.com" },
+// ];
+// const ukrPoshtaUsers = [
+//   { id: "111", userEmail: "tyfany@example.com" },
+//   { id: "222", userEmail: "kirill_22@examle.com" },
+//   { id: "333", userEmail: "linkoln@examle.com" },
+//   { id: "3443", userEmail: "linkoln2222@examle.com" },
+// ];
 
 function App() {
   return (
     <div>
       <MailBox
-        boxUsers={meestExpressUsers}
+        users={meestExpressUsers}
         boxTitle="Meest Express"
         mailBoxCount={3}
       />
       <MailBox
-        boxUsers={novaPoshtaUsers}
+        users={novaPoshtaUsers}
         boxTitle="Nova Poshta"
         mailBoxCount={5}
       />
       <MailBox
-        boxUsers={ukrPoshtaUsers}
+        users={ukrPoshtaUsers}
         boxTitle="UKR Poshta"
       />
     </div>
