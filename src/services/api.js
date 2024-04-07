8968c71e2126d21db7b3e98e5aea462d9ff2c8bf
Roman Fedevych <fedevychr@gmail.com>
2024-04-08 01:26:40 +0300

    http request

---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://dummyjson.com',
+});
+
+// const instance2 = axios.create({
+//   baseURL: 'http://qwerty.com',
+// });
+
+export const requestProducts = async () => {
+  const { data } = await instance.get('/products');
+
+  return data;
+};
+
+export const requestProductsByQuery = async (query = '') => {
+  const { data } = await instance.get(`/products/search?q=${query}`);
+
+  return data;
+};
+
+// export const requestProductsOnAnotherApi = async (query = '') => {
+//   const { data } = await instance2.get(`/products/search?q=${query}`);
+
+//   return data;
+// };
