fb0330b401f287243083f477c5ac5c65d7e3f0cf
Roman Fedevych <fedevychr@gmail.com>
2024-05-29 20:24:12 +0300

    add apiGetProducts and error

--- src/redux/productDetails/productDetailsSlice.js
+++ src/redux/productDetails/productDetailsSlice.js
@@ -1,11 +1,25 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { requestProductsDetailsById } from '../../services/api';
+import { requestProductDetailsById, requestProducts } from '../../services/api';
 
 export const apiRequestProductDetailsById = createAsyncThunk(
   'productDetails/get',
   async (productId, thunkApi) => {
     try {
-      const data = await requestProductsDetailsById(productId);
+      throw new Error('some error 123');
+      const data = await requestProductDetailsById(productId);
+
+      return data; // Те, що повертається з санки потрапляє в action.payload
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.massage);
+    }
+  },
+);
+
+export const apiGetProducts = createAsyncThunk(
+  'products/getAll',
+  async (_, thunkApi) => {
+    try {
+      const data = await requestProducts();
 
       return data; // Те, що повертається з санки потрапляє в action.payload
     } catch (error) {
@@ -16,15 +30,19 @@ export const apiRequestProductDetailsById = createAsyncThunk(
 
 const INITIAL_STATE = {
   productDetails: null,
+  products: null,
   isLoading: false,
   isError: false,
+  error: null,
 };
 
 const productDetailsSlice = createSlice({
   name: 'productDetails',
   initialState: INITIAL_STATE,
+  reducers: {},
   extraReducers: builder =>
     builder
+      // GET PRODUCT DETAILS BY ID
       .addCase(apiRequestProductDetailsById.pending, state => {
         state.isLoading = true;
         state.isError = false;
@@ -33,7 +51,22 @@ const productDetailsSlice = createSlice({
         state.isLoading = false;
         state.productDetails = action.payload;
       })
-      .addCase(apiRequestProductDetailsById.rejected, state => {
+      .addCase(apiRequestProductDetailsById.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.payload;
+      })
+
+      // GET ALL PRODUCTS
+      .addCase(apiGetProducts.pending, state => {
+        state.isLoading = true;
+        state.isError = false;
+      })
+      .addCase(apiGetProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.products = action.payload.products;
+      })
+      .addCase(apiGetProducts.rejected, state => {
         state.isLoading = false;
         state.isError = true;
       }),
